62309a9383961f89208a36570466a36da8186738
VanSandhu <vsandhu1987@gmail.com>
2017-08-18 12:37:32 -0400

    Update 7.Random_TSP_model.R

--- Scripts/7.Random_TSP_model.R
+++ Scripts/7.Random_TSP_model.R
@@ -10,14 +10,14 @@ args(SWAP.Train.KTSP)
 require(data.table)
 library(reportROC)
 
-load("/Users/vandanasandhu/Documents/Projects/Project2_MetadataSubtyping/Survival_cohorts/OS_Predictor_updated_PDAC_no_duplicates.RData")
+load("/Users/vandanasandhu/Documents/Projects/Project2_MetadataSubtyping/Survival_cohorts/OS_Predictor_Aug_15_2017.RData")
+load("/Users/vandanasandhu/Desktop/Project1-Metadatasubtyping/Data_R_objects/random_classifier.RData")
 
-tran_dd=data.frame(t(dd))
 
 ###################### PCSI cohort as training cohort
 ######################
 
-pcsi_cohort=tran_val_coh[which(tran_val_coh$cohort %in% c("PCSI")),]
+pcsi_cohort=val_coh$PCSI
 
 g1=which(as.numeric(as.character(pcsi_cohort$OS))<=365 &  as.numeric(as.character(pcsi_cohort$OS_Status))==1); g2=which(as.numeric(as.character(pcsi_cohort$OS))>365)
 g_ind=sort(c(g1,g2))
@@ -66,7 +66,7 @@ for(i in 1:models_no){
   selected_model[[count]]$TSPs[,2]=sample(colnames(pcsi_mat),length(selected_model[[count]]$TSPs[,1]))
   
   count=count+1;
-  
+  print(i)
 }
 
 
@@ -105,7 +105,7 @@ moff_list=predict_ktsp(moff_mat, moff_grp)
 ouh_list=predict_ktsp(ouh_mat, ouh_grp)
 zhang_list=predict_ktsp(zhang_mat, zhang_grp)
 winter_list=predict_ktsp(winter_mat, winter_grp)
-
+icgc_arr_list=predict_ktsp(icgc_array_mat,icgc_array_grp )
 ###################### Calculating meta-estimates for all the 1000 models using all the cohorts
 
 meta_auc=list()
@@ -113,17 +113,17 @@ seq_auc=list()
 microarray_auc=list()
 
 for( i in 1:1000){
-  meta_auc[[i]] = combine.est(c(icgc_list[[2]][i], tcga_list[[2]][i], moff_list[[2]][i], zhang_list[[2]][i],winter_list[[2]][i], ouh_list[[2]][i]), c( icgc_list[[3]][i], tcga_list[[3]][i], moff_list[[3]][i], zhang_list[[3]][i], winter_list[[3]][i], ouh_list[[3]][i]),na.rm=TRUE)$estimate
+  meta_auc[[i]] = combine.est(c(icgc_list[[2]][i], tcga_list[[2]][i], moff_list[[2]][i], zhang_list[[2]][i],winter_list[[2]][i], ouh_list[[2]][i], icgc_arr_list[[2]][i]), c( icgc_list[[3]][i], tcga_list[[3]][i], moff_list[[3]][i], zhang_list[[3]][i], winter_list[[3]][i], ouh_list[[3]][i],icgc_arr_list[[3]][i]),na.rm=TRUE)$estimate
   seq_auc[[i]] = combine.est(c(icgc_list[[2]][i], tcga_list[[2]][i]), c( icgc_list[[3]][i], tcga_list[[3]][i]),na.rm=TRUE)$estimate
-  microarray_auc[[i]] = combine.est(c( moff_list[[2]][i], zhang_list[[2]][i],winter_list[[2]][i], ouh_list[[2]][i]), c( moff_list[[3]][i], zhang_list[[3]][i], winter_list[[3]][i], ouh_list[[3]][i]),na.rm=TRUE)$estimate
+  microarray_auc[[i]] = combine.est(c( moff_list[[2]][i], zhang_list[[2]][i],winter_list[[2]][i], ouh_list[[2]][i],icgc_arr_list[[2]][i]), c( moff_list[[3]][i], zhang_list[[3]][i], winter_list[[3]][i], ouh_list[[3]][i],icgc_arr_list[[3]][i]),na.rm=TRUE)$estimate
   
 }
 
 ######## Plotting the density plot 
 
-
+pdf("/Users/vandanasandhu/Desktop/Project1-Metadatasubtyping/Figures/Figure6b.pdf")
 myData <-data.frame(Overall=unlist(meta_auc), Sequencing =unlist(seq_auc), Microarray= unlist(microarray_auc)) 
 data=melt(myData)
 colnames(data)=c("Platforms","AUCs")
-ggplot(data,aes(x=AUCs, fill=Platforms)) + geom_density(alpha = 0.3, position = "stack") + geom_vline(aes(xintercept=0.72), color="pink", linetype="dashed", size=1)+ geom_vline(aes(xintercept=0.79), color="green", linetype="dashed", size=1) + geom_vline(aes(xintercept=0.68), color="lightblue", linetype="dashed", size=1)
+ggplot(data,aes(x=AUCs, fill=Platforms)) + geom_density(alpha = 0.3, position = "stack") +  geom_vline(aes(xintercept=0.72), color="pink", linetype="dashed", size=1)+ geom_vline(aes(xintercept=0.77), color="green", linetype="dashed", size=1) + geom_vline(aes(xintercept=0.68), color="lightblue", linetype="dashed", size=1)
 
