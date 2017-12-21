8f9480070878951f91f407ec6deb3a0369474b8e
VanSandhu <vsandhu1987@gmail.com>
2017-12-21 15:19:29 -0500

    Update 6.Clinical_model_comparison.R

--- Scripts/6.Clinical_model_comparison.R
+++ Scripts/6.Clinical_model_comparison.R
@@ -1,5 +1,8 @@
 load("/Users/vandanasandhu/Desktop/RS_Remodelling_TSP_project/clinical_features_ICGC_prob1.RData")
+load("/Users/vandanasandhu/Desktop/RS_Remodelling_TSP_project/Github/RData/cohorts_subtypes_Average_data.RData")
 library(verification)
+library(reportROC)
+library(glmnet)
 ############################## ###########################################
 
 pcsi=data.frame(clinical_features$pcsi_clinical)
@@ -14,18 +17,13 @@ icgc$binary_grp = as.numeric(as.character(icgc$binary_grp))
 icgc$T_status=gsub(" ", "", icgc$T_status, fixed = TRUE)
 
 ##### Model building comparisons
-model1 <- lm(binary_grp ~ Age + Sex + T_status+ N + M + Grade, data=icgc , na.action = na.exclude)
-model2 <- lm(binary_grp ~ pred_prob, data=icgc , na.action = na.exclude)
-model3 <- lm(binary_grp ~ Age + Sex + T_status+ N + M + Grade +pred_prob, data=icgc , na.action = na.exclude)
+model1 <- glm(binary_grp ~ Age + Sex + T_status+ N + M + Grade, data=icgc , na.action = na.exclude,family = binomial(link = "logit"))
+model3 <- glm(binary_grp ~ Age + Sex + T_status+ N + M + Grade + pred_prob, data=icgc , na.action = na.exclude,family = binomial(link = "logit"))
 
-summary(model1); anova(model1)
-summary(model3); anova(model3)
-
-####### Predicting probabilities in training cohort
-icgc_cl_pred1=predict(model1,icgc, na.action = na.exclude)
-icgc_cl_pred2=icgc$pred_prob[which(icgc$ID %in% names(icgc_cl_pred1))]
-icgc_cl_pred3=predict(model3,icgc, na.action = na.exclude)
-
+summary(model1); 
+summary(model3);
+anova(model1);
+anova(model3);
 ####### Predicting probabilities in validation cohorts
 
 ########## 1. PCSI
@@ -36,9 +34,8 @@ pcsi$binary_grp = as.numeric(as.character(pcsi$binary_grp))
 pcsi$T_status = as.character(pcsi$T_status)
 
 
-pcsi_cl_pred1=predict(model1, pcsi, na.action = na.exclude)
+pcsi_cl_pred1=predict(model1, pcsi, na.action = na.exclude,type="response")
 pcsi_cl_pred2=1-pcsi$pred_prob[which(pcsi$ID %in% names(pcsi_cl_pred1))]
-pcsi_cl_pred3=predict(model3, pcsi, na.action = na.exclude)
 
 ########## 2. TCGA
 
@@ -49,9 +46,8 @@ tcga$T_status = as.character(tcga$T_status)
 tcga$pred_prob=as.numeric(as.character(tcga$pred_prob))
 #tcga$Grade=as.character(tcga$Grade)
 
-tcga_cl_pred1=predict(model1, tcga, na.action = na.exclude)
+tcga_cl_pred1=predict(model1, tcga, na.action = na.exclude,type="response")
 tcga_cl_pred2=1-tcga$pred_prob[which(tcga$ID %in% names(tcga_cl_pred1))]
-tcga_cl_pred3=predict(model3, tcga, na.action = na.exclude)
 
 ########## 3. ICGC array
 
@@ -63,9 +59,8 @@ icgc_arr$T_status = as.character(icgc_arr$T_status)
 
 icgc_arr1=icgc_arr[order(icgc_arr$ID),]
 
-icgc_arr_cl_pred1 = predict(model1, icgc_arr, na.action = na.exclude)
+icgc_arr_cl_pred1 = predict(model1, icgc_arr, na.action = na.exclude,type="response")
 icgc_arr_cl_pred2 = 1-icgc_arr$pred_prob[which(icgc_arr1$ID %in% names(icgc_arr_cl_pred1))]
-icgc_arr_cl_pred3 = predict(model3, icgc_arr, na.action = na.exclude)
 
 ########## 4.OUH
 
@@ -73,9 +68,8 @@ ouh$Age=as.numeric(as.character(ouh$Age))
 ouh$Sex = as.character(ouh$Sex)
 ouh$binary_grp = as.numeric(as.character(ouh$binary_grp))
 
-ouh_cl_pred1 = predict(model1, ouh, na.action = na.exclude)
+ouh_cl_pred1 = predict(model1, ouh, na.action = na.exclude,type="response")
 ouh_cl_pred2 = 1-ouh$pred_prob
-ouh_cl_pred3 = predict(model3, ouh, na.action = na.exclude)
 
 
 ######## Model AUCS ESTIMATION ####################
@@ -90,42 +84,32 @@ tcga_roc2=reportROC(tcga[names(tcga_cl_pred1),]$binary_grp,tcga_cl_pred2); tcga_
 ouh_roc2=reportROC(ouh[names(ouh_cl_pred1),]$binary_grp,ouh_cl_pred2); ouh_roc_se2=ouh_roc2$AUC.SE
 icgc_array_roc2 = reportROC(icgc_arr[names(icgc_arr_cl_pred1),]$binary_grp,icgc_arr_cl_pred2);icgc_array_roc_se2 = icgc_array_roc2$AUC.SE
 
-pcsi_roc3=reportROC(pcsi[names(pcsi_cl_pred3),]$binary_grp,pcsi_cl_pred3); pcsi_roc_se3=pcsi_roc3$AUC.SE
-tcga_roc3=reportROC(tcga[names(tcga_cl_pred3),]$binary_grp,tcga_cl_pred3); tcga_roc_se3=tcga_roc3$AUC.SE
-ouh_roc3=reportROC(ouh[names(ouh_cl_pred3),]$binary_grp,ouh_cl_pred3); ouh_roc_se3=ouh_roc3$AUC.SE
-icgc_array_roc3 = reportROC(icgc_arr[names(icgc_arr_cl_pred3),]$binary_grp,icgc_arr_cl_pred3); icgc_array_roc_se3 = icgc_array_roc3$AUC.SE
-
-
 pcsi_roc1_pval= roc.area(pcsi[names(pcsi_cl_pred1),]$binary_grp,pcsi_cl_pred1)$p.value  
 tcga_roc1_pval= roc.area(tcga[names(tcga_cl_pred1),]$binary_grp,tcga_cl_pred1)$p.value  
 ouh_roc1_pval= roc.area(ouh[names(ouh_cl_pred1),]$binary_grp,ouh_cl_pred1)$p.value  
 icgc_array_roc1_pval= roc.area(icgc_arr[names(icgc_arr_cl_pred1),]$binary_grp,icgc_arr_cl_pred1)$p.value  
 
-pcsi_roc2_pval= roc.area(pcsi[names(pcsi_cl_pred1),]$binary_grp,1-pcsi_cl_pred2)$p.value  
+pcsi_roc2_pval= roc.area(pcsi[names(pcsi_cl_pred1),]$binary_grp,pcsi_cl_pred2)$p.value  
 tcga_roc2_pval= roc.area(tcga[names(tcga_cl_pred1),]$binary_grp,tcga_cl_pred2)$p.value  
 ouh_roc2_pval= roc.area(ouh[names(ouh_cl_pred1),]$binary_grp,ouh_cl_pred2)$p.value  
 icgc_array_roc2_pval= roc.area(icgc_arr[names(icgc_arr_cl_pred1),]$binary_grp,icgc_arr_cl_pred2)$p.value  
 
-pcsi_roc3_pval= roc.area(pcsi[names(pcsi_cl_pred3),]$binary_grp,pcsi_cl_pred3)$p.value  
-tcga_roc3_pval= roc.area(tcga[names(tcga_cl_pred3),]$binary_grp,tcga_cl_pred3)$p.value  
-ouh_roc3_pval= roc.area(ouh[names(ouh_cl_pred3),]$binary_grp,ouh_cl_pred3)$p.value  
-icgc_array_roc3_pval= roc.area(icgc_arr[names(icgc_arr_cl_pred3),]$binary_grp,icgc_arr_cl_pred3)$p.value  
-
 
 ######## Plotting barplot
-data <- structure(list("TCGA"= c(tcga_roc1$AUC, tcga_roc2$AUC,tcga_roc3$AUC ),
-                       "PCSI" = c(pcsi_roc1$AUC, pcsi_roc2$AUC, pcsi_roc3$AUC), 
-                       "ICGC-array" = c(icgc_array_roc1$AUC,icgc_array_roc2$AUC,icgc_array_roc3$AUC),
-                       "OUH" = c(ouh_roc1$AUC,ouh_roc2$AUC, ouh_roc3$AUC)), 
-                  .Names = c("TCGA","PCSI","ICGC-array","OUH"), class = "data.frame", row.names = c( "Clinicopathological model", "OS-TSP model", "Integrated clinical and OS-TSP model"))
+data <- structure(list("TCGA"= c(tcga_roc1$AUC, tcga_roc2$AUC ),
+                       "PCSI" = c(pcsi_roc1$AUC, pcsi_roc2$AUC), 
+                       "ICGC-array" = c(icgc_array_roc1$AUC,icgc_array_roc2$AUC),
+                       "OUH" = c(ouh_roc1$AUC,ouh_roc2$AUC)), 
+                  .Names = c("TCGA","PCSI","ICGC-array","OUH"), class = "data.frame", row.names = c( "Clinicopathological model", "PCOSP"))
 
 attach(data)
 print(data)
 
-colours <- c( "lightcyan2",  "cyan3","gray" )
+colours <- c("palevioletred1", "darkgrey")
 #pdf("/Users/vandanasandhu/Desktop/e.pdf")
 
-barplot(as.matrix(data), main="", ylim= c(0, 0.8), ylab = "AUCs",  cex.main = 1.4, beside=TRUE, col=colours,border = 'NA',space=c(0.6,0.08,0.08,0.6,0.08,0.08,0.6,0.08,0.08,0.6,0.08,0.08))
+barplot(as.matrix(data), main="", ylim= c(0, 0.8), ylab = "AUCs",  cex.main = 1.4, beside=TRUE, col=colours,border = 'NA',
+        space=c(0.6,0.08,0.6,0.08,0.6,0.08,0.6,0.08))
 
 ########## Meta-estimate Calculations
 model1_meta = combine.est(c(pcsi_roc1$AUC, tcga_roc1$AUC, ouh_roc1$AUC,icgc_array_roc1$AUC), c( pcsi_roc_se1, tcga_roc_se1, ouh_roc_se1,icgc_array_roc_se1),na.rm=TRUE,hetero=TRUE)$estimate
@@ -136,28 +120,49 @@ model2_meta = combine.est(c(pcsi_roc2$AUC, tcga_roc2$AUC, ouh_roc2$AUC,icgc_arra
 model2_seq = combine.est(c(pcsi_roc2$AUC, tcga_roc2$AUC), c( pcsi_roc_se2, tcga_roc_se2),na.rm=TRUE,hetero=FALSE)$estimate
 model2_micro =combine.est(c( ouh_roc2$AUC,icgc_array_roc2$AUC), c( ouh_roc_se2,icgc_array_roc_se2),hetero=FALSE,na.rm=TRUE)$estimate
 
-model3_meta = combine.est(c(pcsi_roc3$AUC, tcga_roc3$AUC, ouh_roc3$AUC,icgc_array_roc3$AUC), c( pcsi_roc_se3, tcga_roc_se3, ouh_roc_se3,icgc_array_roc_se3),hetero=TRUE,na.rm=TRUE)$estimate
-model3_seq = combine.est(c(pcsi_roc3$AUC, tcga_roc3$AUC), c( pcsi_roc_se3, tcga_roc_se3),na.rm=TRUE,hetero=FALSE)$estimate
-model3_micro =combine.est(c( ouh_roc3$AUC,icgc_array_roc3$AUC), c( ouh_roc_se3,icgc_array_roc_se3),na.rm=TRUE,hetero=FALSE)$estimate
-
 model1_meta_pval <- combine.test(p=c(pcsi_roc1_pval,tcga_roc1_pval, ouh_roc1_pval,icgc_array_roc1_pval),w=c(length(pcsi_cl_pred1),length(tcga_cl_pred1),length(ouh_cl_pred1),length(icgc_arr_cl_pred1)),method="z.transform")
 model1_seq_pval <- combine.test(p=c(pcsi_roc1_pval,tcga_roc1_pval),w=c(length(pcsi_cl_pred1),length(tcga_cl_pred1)),method="z.transform")
 model1_micro_pval <- combine.test(p=c( ouh_roc1_pval,icgc_array_roc1_pval),w=c(length(ouh_cl_pred1),length(icgc_arr_cl_pred1)),method="z.transform")
 
-model2_meta_pval <- combine.test(p=c(pcsi_roc2_pval,tcga_roc2_pval, ouh_roc2_pval,icgc_array_roc2_pval),w=c(length(icgc_cl_pred2),length(tcga_cl_pred2),length(ouh_cl_pred2),length(icgc_arr_cl_pred2)),method="z.transform")
-model2_seq_pval <- combine.test(p=c(pcsi_roc2_pval,tcga_roc2_pval),w=c(length(icgc_cl_pred2),length(tcga_cl_pred2)),method="z.transform")
+model2_meta_pval <- combine.test(p=c(pcsi_roc2_pval,tcga_roc2_pval, ouh_roc2_pval,icgc_array_roc2_pval),w=c(length(pcsi_cl_pred2),length(tcga_cl_pred2),length(ouh_cl_pred2),length(icgc_arr_cl_pred2)),method="z.transform")
+model2_seq_pval <- combine.test(p=c(pcsi_roc2_pval,tcga_roc2_pval),w=c(length(pcsi_cl_pred2),length(tcga_cl_pred2)),method="z.transform")
 model2_micro_pval <- combine.test(p=c( ouh_roc2_pval,icgc_array_roc2_pval),w=c(length(ouh_cl_pred2),length(icgc_arr_cl_pred2)),method="z.transform")
 
-model3_meta_pval <- combine.test(p=c(pcsi_roc3_pval,tcga_roc3_pval, ouh_roc3_pval,icgc_array_roc3_pval),w=c(length(pcsi_cl_pred3),length(tcga_cl_pred3),length(ouh_cl_pred3),length(icgc_arr_cl_pred3)),method="z.transform")
-model3_seq_pval <- combine.test(p=c(pcsi_roc3_pval,tcga_roc3_pval),w=c(length(pcsi_cl_pred3),length(tcga_cl_pred3)),method="z.transform")
-model3_micro_pval <- combine.test(p=c( ouh_roc3_pval,icgc_array_roc3_pval),w=c(length(ouh_cl_pred3),length(icgc_arr_cl_pred3)),method="z.transform")
+
+
+##################################
+cohort_classes$pcsi[,1] == pcsi$ID
+
+pcsi_classes= cohort_classes$pcsi[,2][which(cohort_classes$pcsi[,1] %in% pcsi$ID)]
+cohort_classes$pcsi[,1][which(cohort_classes$pcsi[,1] %in% pcsi$ID)]==pcsi$ID
+pcsi_grp=ifelse(as.numeric(as.character(pcsi$OS)) >=365,1,0)
+
+tcga$ID=gsub( "_",".",tcga$ID)
+tcga_classes= cohort_classes$tcga[,2][which(cohort_classes$tcga[,1] %in% tcga$ID)]
+cohort_classes$tcga[,1][which(cohort_classes$tcga[,1] %in% tcga$ID)]==tcga$ID
+tcga_grp=ifelse(as.numeric(as.character(tcga$OS)) >=365,1,0)
+
+
+ouh_classes= cohort_classes$ouh[,2][which(cohort_classes$ouh[,1] %in% ouh$ID)]
+cohort_classes$ouh[,1][which(cohort_classes$ouh[,1] %in% ouh$ID)]==ouh$ID
+ouh_grp=ifelse(as.numeric(as.character(ouh$OS)) >=365,1,0)
+
+icgc_arr_classes= cohort_classes$icgc_arr[,2][which(cohort_classes$icgc_arr[,1] %in% icgc_arr$ID)]
+cohort_classes$icgc_arr[,1][which(cohort_classes$icgc_arr[,1] %in% icgc_arr$ID)]==icgc_arr$ID
+icgc_arr_grp=ifelse(as.numeric(as.character(icgc_arr$OS)) >=365,1,0)
+
+
+icgc_classes= cohort_classes$icgc[,2][which(cohort_classes$icgc[,1] %in% icgc$ID)]
+cohort_classes$icgc[,1][which(cohort_classes$icgc[,1] %in% icgc$ID)]==icgc$ID
+icgc_seq_grp=ifelse(as.numeric(as.character(icgc$OS)) >=365,1,0)
 
 
 
-
-
-
-
+summary(glm(ouh_grp ~ Age + Sex + T_status+ N + M + Grade +pred_prob+ouh_classes, data=ouh, na.action = na.exclude))
+summary(glm(pcsi_grp ~ Age + Sex + T_status+ N + M + Grade + pred_prob + pcsi_classes, data=pcsi, na.action = na.exclude))
+summary(glm(tcga_grp ~ Age + Sex + T_status+ N + M + Grade + pred_prob +tcga_classes , data=tcga, na.action = na.exclude))
+summary(glm(icgc_arr_grp ~ Age + Sex + T_status+ N + M + Grade + pred_prob + icgc_arr_classes, data=icgc_arr, na.action = na.exclude))
+summary(glm(icgc_seq_grp ~ Age + Sex + T_status+ N + M + Grade + pred_prob + icgc_classes , data=icgc , na.action = na.exclude))
 
 
 
